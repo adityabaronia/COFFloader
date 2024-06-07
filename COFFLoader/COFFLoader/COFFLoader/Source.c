c7cc6cceb743e0e31a0defb0b22c7d1cf52ed4d6
smokiieee <57633388+adityabaronia@users.noreply.github.com>
2024-06-07 12:17:00 +0530

    Update Source.c

--- COFFLoader/COFFLoader/COFFLoader/Source.c
+++ COFFLoader/COFFLoader/COFFLoader/Source.c
@@ -2,24 +2,69 @@
 #include <stdio.h>
 
 typedef struct{
-	WORD machine; //uint16_t
-	BYTE numberOfSection; //uint16_t
-	DWORD timeStamp; //uint32_t
-	DWORD filePtrSmblTbl; // file pointer to symbol table //uint32_t
-	WORD sizeOfOptionalHeader;  //uint16_t
-	WORD characteristics; //uint16_t
-};
+	UINT16 machine; //uint16_t
+	UINT8 numberOfSection; //uint16_t
+	UINT32 timeStamp; //uint32_t
+	UINT32 filePtrSmblTbl; // file pointer to symbol table //uint32_t
+	UINT16 noOfSymbols; //uint16_t
+	UINT16 sizeOfOptionalHeader;  //uint16_t
+	UINT16 characteristics; //uint16_t
+}fileHeader;
 
 
+typedef struct {
+	char Name[8];
+	UINT32 VirtualSize;
+	UINT32 VirtualAddress;
+	UINT32 SizeOfRawData;
+	UINT32 PointerToRawData;
+	UINT32 PointerToRelocations;
+	UINT32 PointerToLinenumbers;
+	UINT16 NumberOfRelocations;
+	UINT16 NumberOfLinenumbers;
+	UINT32 Characteristics;
+}sectionHeader;
 
+void printFileHeader(fileHeader* fheader) {
+	printf("FILEHEADER:\n");
+	printf("machine: 0x%x\n", fheader->machine);
+	printf("Number of section: 0x%x\n", fheader->numberOfSection);
+	printf("Time and date stamp: 0x%x\n", fheader->timeStamp);
+	printf("filepointer to symbol table: 0x%x\n", fheader->filePtrSmblTbl);
+	printf("number of symbols: 0x%x\n", fheader->noOfSymbols);
+	printf("size of optionl header: 0x%x\n", fheader->sizeOfOptionalHeader);
+	printf("charactersctics: 0x%x\n", fheader->characteristics);
+	printf("\n");
+}
 
-int main() {
+void printSectionHeader(sectionHeader* section_n) {
+	printf("Name of section header: %s\n", (section_n->Name));
+	printf("Virtual size of section header is 0x%x\n", section_n->VirtualSize);
+	printf("virtual address is 0x%x\n", section_n->VirtualAddress);
+	printf("size of raw raw data is 0x%x\n", section_n->SizeOfRawData);
+	printf("pointer to raw data is 0x%x\n", section_n->PointerToRawData);
+	printf("Pointer to relocations is 0x%x\n", section_n->PointerToRelocations);
+	printf("Pointer to linenumbers is 0x%x\n", section_n->PointerToLinenumbers);
+	printf("Number of relocations is 0x%x\n", section_n->NumberOfRelocations);
+	printf("Number of linenumbers is 0x%x\n", section_n->NumberOfLinenumbers);
+	printf("Characteristics is 0x%x\n", section_n->Characteristics);
+	printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		printf("Usage: COFFloader.exe <path\\of\\obj\\file.obj>\n");
+		exit(1);
+	}
+		printf("File to parse: %s\n\n", argv[1]);
 	HANDLE hFile = NULL;
-	LPDWORD objFileSize = 0;
+	size_t objFileSize = 0;
 	LPVOID objFileInMem = NULL;
 	LPDWORD bytesRead = NULL;
+	fileHeader* fheader;
 	//opening file hadle
-	hFile = CreateFileA("C:\\Users\\smoke.REDTEAMER\\Desktop\\COFFLoader\\helloworld.obj", 
+	
+	hFile = CreateFileA(argv[1],
 		GENERIC_READ, 
 		0, 
 		NULL, 
@@ -30,14 +75,15 @@ int main() {
 		printf("error in opening object file: %d\n", GetLastError());
 		exit(1);
 	}
-	
-	if (!GetFileSize(hFile, objFileSize)) {
+	objFileSize = GetFileSize(hFile, NULL);
+	if (objFileSize == INVALID_FILE_SIZE) {
 		printf("Could not get file size: %d\n", GetLastError());
 		exit(1);
 	}
+	printf("Object file size is %lld\n", objFileSize);
 
 	//Allocating virtual memory
-	objFileInMem = VirtualAlloc(0,
+	objFileInMem = VirtualAlloc(NULL,
 		objFileSize, 
 		MEM_COMMIT | MEM_RESERVE, 
 		PAGE_READWRITE);
@@ -47,17 +93,23 @@ int main() {
 		exit(1);
 	}
 
-	if (!ReadFile(hFile, objFileInMem, objFileSize, bytesRead, NULL)) {
+	if (FALSE == ReadFile(hFile, objFileInMem, objFileSize, &bytesRead, NULL)) {
 		printf("Error in reading file: %d\n", GetLastError());
 	}
 	if (objFileSize != bytesRead) {
-		printf("Error in reading complete file\n");
+		printf("Bytes read is %ulld .Error in reading complete file\n", bytesRead);
 		exit(1);
 	}
-
-
-
-
-
+	
+	fheader = (fileHeader*)objFileInMem;
+	printFileHeader(fheader);
+	
+	
+	for (int i = 0; i < fheader->numberOfSection; i++) {
+		sectionHeader* section_n = (size_t)objFileInMem
+			+ (size_t)sizeof(fileHeader) +  i * (size_t)sizeof(sectionHeader);;
+		printf("SECTION HEADER: %x\n", i+1);
+		printSectionHeader(section_n);
+	}
 	return 0;
 }
